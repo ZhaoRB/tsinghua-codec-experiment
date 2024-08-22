da67fbb32430e5bdf9fe49785fe5b9aec2a5a838
riverzhao <zrb23@mails.tsinghua.edu.cn>
2024-08-22 11:27:41 +0800

    feat: add config files

--- scripts/format-convert/img2yuv.py
+++ scripts/format-convert/img2yuv.py
@@ -1,15 +1,15 @@
 import subprocess
 
 # 设置输入文件夹的路径
-input_pattern = "path_to_your_frames_directory/frame%03d/image_013.png"
+input_pattern = "/mnt/e/ZRB/240819-4-rendered/frame#%03d/image_013.png"
 
 # 设置输出文件路径
-output_file = "path_to_your_frames_directory/output.yuv"
+output_file = "/mnt/e/ZRB/new_sequence_1378x1012_350frames.yuv"
 
 # 调用 ffmpeg 直接进行转换
 subprocess.run([
     "ffmpeg",
-    "start_number", "0",
+    "-start_number", "1",
     "-i", input_pattern,
     "-frames:v", "300",
     "-pix_fmt", "yuv420p",
