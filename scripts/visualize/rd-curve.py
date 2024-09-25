205f92655d752cc1dbb5341ee8c8bbd16e180f1b
riverzhao <buaariverzhao@163.com>
2024-09-25 15:32:43 +0800

    feat: draw rd curve from csv file

--- scripts/visualize/rd-curve.py
+++ scripts/visualize/rd-curve.py
@@ -6,12 +6,12 @@ import matplotlib.pyplot as plt
 input_folder = "/Users/riverzhao/Project/Codec/3_experiment/tsinghua-codec-experiment/data/find-qp/csv"
 output_folder = "/Users/riverzhao/Project/Codec/3_experiment/tsinghua-codec-experiment/data/find-qp/figure"
 # sequence_names = ["fujita", "origami", "motherboard"]
-sequence_names = ["motherboard"]
+sequence_names = ["origami"]
 
-# qps_to_plot = [26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50]
+qps_to_plot = [26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50]
 qps_to_plot = [32, 36, 40, 44]
-# qps_to_plot = [30, 34, 38, 42]
-qps_to_plot = [36, 40, 44, 48]
+qps_to_plot = [28, 32, 36, 40]
+# qps_to_plot = [36, 40, 44, 48]
 
 
 def qpsToString(qpsArr):
@@ -20,7 +20,9 @@ def qpsToString(qpsArr):
 
 for seq_name in sequence_names:
     input_path = os.path.join(input_folder, f"{seq_name}.csv")
-    output_path_png = os.path.join(output_folder, f"{seq_name}_{qpsToString(qps_to_plot)}.png")
+    output_path_png = os.path.join(
+        output_folder, f"{seq_name}_{qpsToString(qps_to_plot)}.png"
+    )
     # output_path_svg = os.path.join(output_folder, f"{seq_name}.svg")
 
     bitrates = []
