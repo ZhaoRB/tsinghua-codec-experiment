9601388326d25bf4c05fc72f77cc5b6620375e78
riverzhao-codec-server <zrb23@mails.tsinghua.edu.cn>
2025-01-15 14:53:49 +0800

    feat: general test framework

---
+++ general-test-framework/src/run-summary.py
@@ -0,0 +1,126 @@
+import csv
+import os
+
+from initialize import *
+from tasks.format_convert import img2yuv
+from utils.extract import extract_codec_info, if_codec_finish
+from utils.psnr import compute_psnr_for_yuv
+
+summaryLogFile = os.path.join(summaryOutputFolder, "summary.log")
+
+headers = [
+    "Sequence Name",
+    "QP",
+    "Bitrate",
+    "LLPSNR_Y",
+    "LLPSNR_U",
+    "LLPSNR_V",
+    "MVPSNR_Y",
+    "MVPSNR_U",
+    "MVPSNR_V",
+]
+
+
+def run_summary(seq):
+    seqCsvFileName = getSeqCsvFileName(seq)
+
+    with open(seqCsvFileName, mode="w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(headers)
+
+        for i in range(viewNum * viewNum):
+            index = i + 1
+            curImagePattern = f"image_{index:03}.png"
+
+            img2yuv(
+                ffmpeg,
+                startFrame,
+                frames,
+                os.path.join(getBaseRenderFramePattern(seq), curImagePattern),
+                getSummaryTempBaseYuv(seq, index),
+                summaryLogFile,
+            )
+
+        for qp in qps[seq]:
+            print(f"Start summary for {seq} QP {qp}...")
+            
+            # 1. get bitrate and llpsnr from vvc logfile
+            logfile = os.path.join(codecOutputFolder, f"{seq}_qp{qp}.log")
+
+            # check if log file exists and if codec finished
+            if not os.path.exists(logfile) or not if_codec_finish(logfile):
+                continue
+
+            bitrate, llpsnr_y, llpsnr_u, llpsnr_v = extract_codec_info(logfile)
+
+            # 2. calculate mvpsnr from all view points
+            mvpsnr_y, mvpsnr_u, mvpsnr_v = [], [], []
+
+            for i in range(viewNum * viewNum):
+                index = i + 1
+                curImagePattern = f"image_{index:03}.png"
+
+                baseYuv = getSummaryTempBaseYuv(seq, index)
+                curQpYuv = getSummaryTempQpYuv(seq, qp, index)
+
+                img2yuv(
+                    ffmpeg,
+                    startFrame,
+                    frames,
+                    os.path.join(getRenderFramePattern(seq, qp), curImagePattern),
+                    curQpYuv,
+                    summaryLogFile,
+                )
+
+                cur_mvpsnr_y, cur_mvpsnr_u, cur_mvpsnr_v = compute_psnr_for_yuv(
+                    baseYuv,
+                    curQpYuv,
+                    rendered_resolutions[seq][0],
+                    rendered_resolutions[seq][1],
+                )
+
+                mvpsnr_y.append(cur_mvpsnr_y)
+                mvpsnr_u.append(cur_mvpsnr_u)
+                mvpsnr_v.append(cur_mvpsnr_v)
+
+            avg_mvpsnr_y, avg_mvpsnr_u, avg_mvpsnr_v = (
+                sum(mvpsnr_y) / len(mvpsnr_y),
+                sum(mvpsnr_u) / len(mvpsnr_u),
+                sum(mvpsnr_v) / len(mvpsnr_v),
+            )
+
+            # Write data row for the current sequence and qp value
+            writer.writerow(
+                [
+                    seq,
+                    qp,
+                    bitrate,
+                    llpsnr_y,
+                    llpsnr_u,
+                    llpsnr_v,
+                    avg_mvpsnr_y,
+                    avg_mvpsnr_u,
+                    avg_mvpsnr_v,
+                ]
+            )
+
+
+def mergeAllCsvFiles():
+    with open(csvFileNameAllSeqs, mode="w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(headers)
+
+    for seq in seqs:
+        if os.path.exists(getSeqCsvFileName(seq)):
+            with open(getSeqCsvFileName(seq), mode="r", newline="") as csvfile:
+                reader = csv.reader(csvfile)
+                next(reader)  # Skip header row
+                for row in reader:
+                    writer.writerow(row)
+
+
+if __name__ == "__main__":
+    for seq in seqs:
+        run_summary(seq)
+
+    # mergeAllCsvFiles()
---
+++ general-test-framework/src/utils/extract.py
@@ -0,0 +1,35 @@
+import re
+
+def if_codec_finish(logFile):
+    target_line = "Total Frames |   Bitrate     Y-PSNR    U-PSNR    V-PSNR    YUV-PSNR"
+    with open(logFile, "r") as file:
+        for line in file:
+            if target_line in line:
+                return True
+    return False
+
+
+def extract_codec_info(logFilePath):
+
+    # 读取log文件内容
+    with open(logFilePath, "r") as file:
+        log_content = file.read()
+
+    # 定义匹配的正则表达式模式
+    pattern = r"(\d+)\s+[a-zA-Z]+\s+([0-9]+\.[0-9]+)\s+([0-9]+\.[0-9]+)\s+([0-9]+\.[0-9]+)\s+([0-9]+\.[0-9]+)\s+([0-9]+\.[0-9]+)"
+
+    # 搜索匹配的内容
+    match = re.search(pattern, log_content)
+
+    if match:
+        # total_frames = int(match.group(1))
+        bitrate = float(match.group(2))
+        y_psnr = float(match.group(3))
+        u_psnr = float(match.group(4))
+        v_psnr = float(match.group(5))
+        # yuv_psnr = float(match.group(6))
+        return [bitrate, y_psnr, u_psnr, v_psnr]
+
+    else:
+        print("No matching data found.")
+        return []
